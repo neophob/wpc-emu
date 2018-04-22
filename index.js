a68cad93b3c88c333266598a2c559399c18479e6
michuNEEO <michael@neeo.com>
2018-04-22 23:08:07 +0200

    add status for blocked event loop

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const wpc = require('./lib/wpc');
 const debug = require('debug')('wpcemu:index');
+const blocked = require('blocked');
 
 const romPath = process.argv[2] || 'rom/HURCNL_2.ROM';
+const ALERT_WHEN_EVENTLOOP_IS_BLOCKED_MS = 100;
 
 if (!romPath) {
   console.error('Parameter [ROM PATH]');
@@ -26,3 +28,7 @@ wpc.initVMwithRom(romPath)
     console.log('EXCEPTION!', error.message);
     console.log(error.stack);
   });
+
+blocked((ms) => {
+  debug('WARNING_EVENT_LOOP_BLOCKED', ms);
+}, { threshold: ALERT_WHEN_EVENTLOOP_IS_BLOCKED_MS });
