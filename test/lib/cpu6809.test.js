f768a4c63c57fc9f79e0fcf77dd98b0ade2b9d1b
michuNEEO <michael@neeo.com>
2018-04-25 20:23:29 +0200

    respect interupt flags before calling them

--- test/lib/cpu6809.test.js
+++ test/lib/cpu6809.test.js
@@ -3,6 +3,8 @@
 import test from 'ava';
 import CPU6809 from '../../lib/cpu6809';
 
+/*jshint bitwise: false*/
+
 let readMemoryAddress, writeMemoryAddress;
 test.beforeEach(() => {
   readMemoryAddress = [];
@@ -22,8 +24,8 @@ test.serial('read initial vector', t => {
   t.is(readMemoryAddress[1], 0xFFFF);
 });
 
-test.serial('flags should be correct after calling irq()', t => {
-  CPU6809.set('flags', 0);
+test.serial('flags should be correct after calling irq(), init flags to 0x00', t => {
+  CPU6809.set('flags', 0x00);
   CPU6809.irq();
   CPU6809.steps();
   t.is(CPU6809.flagsToString(), 'EfhInzvc');
@@ -31,8 +33,26 @@ test.serial('flags should be correct after calling irq()', t => {
   t.is(readMemoryAddress[3], 0xFFF9);
 });
 
+test.serial('flags should be correct after calling irq(), init flags to 0xef', t => {
+  const flagClearedFirqBit = 0xff & ~16;
+  CPU6809.set('flags', flagClearedFirqBit);
+  CPU6809.irq();
+  CPU6809.steps();
+  t.is(readMemoryAddress[2], 0xFFF8);
+  t.is(readMemoryAddress[3], 0xFFF9);
+  t.is(CPU6809.flagsToString(), 'EFHINZVC');
+});
+
+test.serial('irq() should not be called if F_IRQMASK flag is set', t => {
+  CPU6809.set('flags', 0xff);
+  CPU6809.irq();
+  CPU6809.steps();
+  t.is(readMemoryAddress[2], NaN);
+  t.is(readMemoryAddress[3], undefined);
+});
+
 test.serial('flags should be correct after calling nmi()', t => {
-  CPU6809.set('flags', 0);
+  CPU6809.set('flags', 0x00);
   CPU6809.nmi();
   CPU6809.steps();
   t.is(CPU6809.flagsToString(), 'EFhInzvc');
@@ -41,7 +61,7 @@ test.serial('flags should be correct after calling nmi()', t => {
 });
 
 test.serial('flags should be correct after calling firq(), init flags to 0x00', t => {
-  CPU6809.set('flags', 0);
+  CPU6809.set('flags', 0x00);
   CPU6809.firq();
   CPU6809.steps();
   t.is(CPU6809.flagsToString(), 'eFhInzvc');
@@ -49,9 +69,20 @@ test.serial('flags should be correct after calling firq(), init flags to 0x00', 
   t.is(readMemoryAddress[3], 0xFFF7);
 });
 
-test.serial('flags should be correct after calling firq(), init flags to 0xff', t => {
+test.serial('flags should be correct after calling firq(), init flags to 0xbf', t => {
+  const flagClearedFirqBit = 0xff & ~64;
+  CPU6809.set('flags', flagClearedFirqBit);
+  CPU6809.firq();
+  CPU6809.steps();
+  t.is(readMemoryAddress[2], 0xFFF6);
+  t.is(readMemoryAddress[3], 0xFFF7);
+  t.is(CPU6809.flagsToString(), 'eFHINZVC');
+});
+
+test.serial('firq() should not be called if F_FIRQMASK flag is set', t => {
   CPU6809.set('flags', 0xff);
   CPU6809.firq();
   CPU6809.steps();
-  t.is(CPU6809.flagsToString(), 'eFHINZVC');
+  t.is(readMemoryAddress[2], NaN);
+  t.is(readMemoryAddress[3], undefined);
 });
