5ba3ad962fe803c545bb7ade95fed0f0f98552fb
michuNEEO <michael@neeo.com>
2018-04-25 08:48:07 +0200

    add some diagrams to client

--- client/client.js
+++ client/client.js
@@ -96,25 +96,59 @@ downloadFileFromUrlAsUInt8Array('https://s3-eu-west-1.amazonaws.com/foo-temp/hur
     console.log(error.stack);
   });
 
+const YPOS_GENERIC_DATA = 20;
+const YPOS_WPC_DATA = 170;
+const YPOS_DMD_DATA = 320;
+
 function updateCanvas() {
   canvasClear();
   const emuState = wpcSystem.getUiState();
+  c.font = '10px Monaco';
+
+  c.fillStyle = 'magenta';
+  c.fillText('GENERIC DATA:', 0, YPOS_GENERIC_DATA);
+  c.fillText('WPC BOARD DATA:', 0, YPOS_WPC_DATA);
+  c.fillText('DMD BOARD DATA:', 0, YPOS_DMD_DATA);
+
   c.fillStyle = 'yellow';
-  c.font = '12px Monaco';
-  c.fillText('ROM: hurcnl_2.rom', 0, 30);
-  c.fillText('CPU Ticks: ' + emuState.ticks + ' / OPMS: ' + emuState.opMs, 250, 30);
+  c.fillText('ROM: hurcnl_2.rom', 0, YPOS_GENERIC_DATA + 10);
+  c.fillText('CPU Ticks: ' + emuState.ticks + ' / OPMS: ' + emuState.opMs, 250, YPOS_GENERIC_DATA + 10);
   const cpuState = intervalId ? 'running' : 'stopped';
-  c.fillText('CPU state: ' + cpuState, 500, 30);
+  c.fillText('CPU state: ' + cpuState, 500, YPOS_GENERIC_DATA + 10);
+
   const diagnosticLed = emuState.asic.wpc.dignosticLed ? emuState.asic.wpc.dignosticLed.toString(2) : '00000000';
+  c.fillText('DIAGLED: ' + diagnosticLed, 0, YPOS_WPC_DATA + 10);
+  c.fillText('Active ROM Bank: ' + emuState.asic.wpc.activeRomBank, 250, YPOS_WPC_DATA + 10);
+  c.fillText('IRQ enabled: ' + emuState.asic.wpc.irqEnabled, 500, YPOS_WPC_DATA + 10);
 
-  c.fillText('DIAGLED: ' + diagnosticLed, 0, 60);
-  c.fillText('Active ROM Bank: ' + emuState.asic.wpc.activeRomBank, 250, 60);
+  c.fillText('DMD ACTIVE PAGE: ' + emuState.asic.dmd.activepage, 0, YPOS_DMD_DATA + 10);
+  c.fillText('DMD LOW PAGE: ' + emuState.asic.dmd.lowpage, 250, YPOS_DMD_DATA + 10);
+  c.fillText('DMD HIGH PAGE: ' + emuState.asic.dmd.highpage, 500, YPOS_DMD_DATA + 10);
 
-  c.fillText('DMD ACTIVE PAGE: ' + emuState.asic.dmd.activepage, 0, 90);
-  c.fillText('DMD LOW PAGE: ' + emuState.asic.dmd.lowpage, 250, 90);
-  c.fillText('DMD HIGH PAGE: ' + emuState.asic.dmd.highpage, 500, 90);
+  c.fillText('DMD SCANLINE: ' + emuState.asic.dmd.scanline, 0, YPOS_DMD_DATA + 20);
 
-  c.fillText('DMD SCANLINE: ' + emuState.asic.dmd.scanline, 0, 120);
+
+  c.fillText('ASIC RAM:', 0, YPOS_GENERIC_DATA + 30);
+  c.fillText('PAGE 1 RAM:', 0, YPOS_DMD_DATA + 40);
+  c.fillText('PAGE 2 RAM:', 250, YPOS_DMD_DATA + 40);
+
+  drawMemRegion(emuState.asic.ram, 0, YPOS_GENERIC_DATA + 40, 128);
+  drawMemRegion(emuState.asic.dmd.page1, 0, YPOS_DMD_DATA + 50, 128);
+  drawMemRegion(emuState.asic.dmd.page2, 250, YPOS_DMD_DATA + 50, 128);
+}
+
+function drawMemRegion(data, x, y, width) {
+  var offsetX = 0;
+  var offsetY = 0;
+  data.forEach((alpha) => {
+    c.fillStyle = 'rgba(255, 255, 255, ' + alpha + ')';
+    c.fillRect (x + offsetX, y + offsetY, 1, 1);
+    offsetX ++;
+    if (offsetX === width) {
+      offsetX = 0;
+      offsetY ++;
+    }
+  });
 }
 
 function canvasClear() {
