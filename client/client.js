5f31a5d369e0c48820d3ee0ed3e0e8d9b8657062
michuNEEO <michael@neeo.com>
2018-04-26 17:16:11 +0200

    ui, draw larger dmd display

--- client/client.js
+++ client/client.js
@@ -98,59 +98,62 @@ const YPOS_GENERIC_DATA = 20;
 const YPOS_WPC_DATA = 170;
 const YPOS_DMD_DATA = 320;
 
+const LEFT_X_OFFSET = 20;
+const MIDDLE_X_OFFSET = 260 + LEFT_X_OFFSET;
+const RIGHT_X_OFFSET = 260 + MIDDLE_X_OFFSET;
+const RIGHT_PLUS_X_OFFSET = 130 + RIGHT_X_OFFSET;
+
 function updateCanvas() {
   canvasClear();
   const emuState = wpcSystem.getUiState();
   c.font = '10px Monaco';
 
   c.fillStyle = 'magenta';
-  c.fillText('# GENERIC DATA:', 0, YPOS_GENERIC_DATA);
-  c.fillText('# WPC BOARD DATA:', 0, YPOS_WPC_DATA);
-  c.fillText('# DMD BOARD DATA:', 0, YPOS_DMD_DATA);
+  c.fillText('# GENERIC DATA:', LEFT_X_OFFSET, YPOS_GENERIC_DATA);
+  c.fillText('# WPC BOARD DATA:', LEFT_X_OFFSET, YPOS_WPC_DATA);
+  c.fillText('# DMD BOARD DATA:', LEFT_X_OFFSET, YPOS_DMD_DATA);
 
   c.fillStyle = 'yellow';
-  c.fillText('ROM: hurcnl_2.rom', 0, YPOS_GENERIC_DATA + 10);
-  c.fillText('CPU TICKS: ' + emuState.ticks, 250, YPOS_GENERIC_DATA + 10);
-  c.fillText('CPU TICKS/ms: ' + emuState.opMs, 500, YPOS_GENERIC_DATA + 10);
+  c.fillText('ROM: hurcnl_2.rom', LEFT_X_OFFSET, YPOS_GENERIC_DATA + 10);
+  c.fillText('CPU TICKS: ' + emuState.ticks, MIDDLE_X_OFFSET, YPOS_GENERIC_DATA + 10);
+  c.fillText('CPU TICKS/ms: ' + emuState.opMs, RIGHT_X_OFFSET, YPOS_GENERIC_DATA + 10);
   const cpuState = intervalId ? 'running' : 'paused';
-  c.fillText('CPU STATE: ' + cpuState, 250, YPOS_GENERIC_DATA + 20);
-  c.fillText('ASIC RAM:', 500, YPOS_GENERIC_DATA + 20);
+  c.fillText('CPU STATE: ' + cpuState, MIDDLE_X_OFFSET, YPOS_GENERIC_DATA + 20);
+  c.fillText('ASIC RAM:', RIGHT_X_OFFSET, YPOS_GENERIC_DATA + 20);
 
   const diagnosticLed = emuState.asic.wpc.diagnosticLed ? emuState.asic.wpc.diagnosticLed.toString(2) : '00000000';
   const lampRow = emuState.asic.wpc.lampRow;
   const lampColumn = emuState.asic.wpc.lampColumn;
   const switchRow = emuState.asic.wpc.switchRow;
   const switchColumn = emuState.asic.wpc.switchColumn;
-  c.fillText('DIAGLED STATE: ' + diagnosticLed, 0, YPOS_WPC_DATA + 10);
-  c.fillText('DIAGLED TOGGLE COUNT: ' + emuState.asic.wpc.diagnosticLedToggleCount, 250, YPOS_WPC_DATA + 10);
-  c.fillText('LAMP ROW: ' + lampRow, 500, YPOS_WPC_DATA + 10);
-  c.fillText('SWITCH ROW: ' + switchRow, 625, YPOS_WPC_DATA + 10);
+  c.fillText('DIAGLED STATE: ' + diagnosticLed, LEFT_X_OFFSET, YPOS_WPC_DATA + 10);
+  c.fillText('DIAGLED TOGGLE COUNT: ' + emuState.asic.wpc.diagnosticLedToggleCount, MIDDLE_X_OFFSET, YPOS_WPC_DATA + 10);
+  c.fillText('LAMP ROW: ' + lampRow, RIGHT_X_OFFSET, YPOS_WPC_DATA + 10);
+  c.fillText('SWITCH ROW: ' + switchRow, RIGHT_PLUS_X_OFFSET, YPOS_WPC_DATA + 10);
 
-  c.fillText('Active ROM Bank: ' + emuState.asic.wpc.activeRomBank, 0, YPOS_WPC_DATA + 20);
-  c.fillText('IRQ enabled: ' + emuState.asic.wpc.irqEnabled, 250, YPOS_WPC_DATA + 20);
-  c.fillText('LAMP COL: ' + lampColumn, 500, YPOS_WPC_DATA + 20);
-  c.fillText('SWITCH COL: ' + switchColumn, 625, YPOS_WPC_DATA + 20);
+  c.fillText('Active ROM Bank: ' + emuState.asic.wpc.activeRomBank, LEFT_X_OFFSET, YPOS_WPC_DATA + 20);
+  c.fillText('IRQ enabled: ' + emuState.asic.wpc.irqEnabled, MIDDLE_X_OFFSET, YPOS_WPC_DATA + 20);
+  c.fillText('LAMP COL: ' + lampColumn, RIGHT_X_OFFSET, YPOS_WPC_DATA + 20);
+  c.fillText('SWITCH COL: ' + switchColumn, RIGHT_PLUS_X_OFFSET, YPOS_WPC_DATA + 20);
 
-  c.fillText('DMD ACTIVE PAGE: ' + emuState.asic.dmd.activepage, 0, YPOS_DMD_DATA + 10);
-  c.fillText('DMD LOW PAGE: ' + emuState.asic.dmd.lowpage, 250, YPOS_DMD_DATA + 10);
-  c.fillText('DMD HIGH PAGE: ' + emuState.asic.dmd.highpage, 500, YPOS_DMD_DATA + 10);
+  c.fillText('DMD ACTIVE PAGE: ' + emuState.asic.dmd.activepage, LEFT_X_OFFSET, YPOS_DMD_DATA + 10);
+  c.fillText('DMD LOW PAGE: ' + emuState.asic.dmd.lowpage, MIDDLE_X_OFFSET, YPOS_DMD_DATA + 10);
+  c.fillText('DMD HIGH PAGE: ' + emuState.asic.dmd.highpage, RIGHT_X_OFFSET, YPOS_DMD_DATA + 10);
 
-  c.fillText('DMD SCANLINE: ' + emuState.asic.dmd.scanline, 0, YPOS_DMD_DATA + 20);
+  c.fillText('DMD SCANLINE: ' + emuState.asic.dmd.scanline, LEFT_X_OFFSET, YPOS_DMD_DATA + 20);
 
-  c.fillText('PAGE 1 RAM:', 0, YPOS_DMD_DATA + 40);
-  c.fillText('PAGE 2 RAM:', 250, YPOS_DMD_DATA + 40);
+  c.fillText('PAGE 1 RAM:', LEFT_X_OFFSET, YPOS_DMD_DATA + 40);
+  c.fillText('PAGE 2 RAM:', MIDDLE_X_OFFSET, YPOS_DMD_DATA + 40);
 
   //TODO payload is wrong
-  drawMatrix(lampRow, lampColumn, 500, YPOS_WPC_DATA + 30);
-  drawMatrix(switchRow, switchColumn, 625, YPOS_WPC_DATA + 30);
+  drawMatrix(lampRow, lampColumn, RIGHT_X_OFFSET, YPOS_WPC_DATA + 30);
+  drawMatrix(switchRow, switchColumn, RIGHT_PLUS_X_OFFSET, YPOS_WPC_DATA + 30);
 
-  drawMemRegion(emuState.asic.ram, 500, YPOS_GENERIC_DATA + 20, 128);
+  drawMemRegion(emuState.asic.ram, RIGHT_X_OFFSET, YPOS_GENERIC_DATA + 20, 128);
 
   //dmd pages - 8 pixel (on/off) per byte, display is 128x32 pixels
-  drawMemRegion(emuState.asic.dmd.page1, 0, YPOS_DMD_DATA + 40, 16);
-  drawMemRegion(emuState.asic.dmd.page2, 250, YPOS_DMD_DATA + 40, 16);
-  drawDmd(emuState.asic.dmd.page1, 0, 400, 16*8);
-  drawDmd(emuState.asic.dmd.page2, 250, 400, 16*8);
+  drawDmd(emuState.asic.dmd.page1, LEFT_X_OFFSET, YPOS_DMD_DATA + 40, 16*8);
+  drawDmd(emuState.asic.dmd.page2, MIDDLE_X_OFFSET, YPOS_DMD_DATA + 40, 16*8);
 }
 
 function drawMemRegion(data, x, y, width) {
@@ -200,7 +203,7 @@ function drawMatrix(row, column, x, y) {
 
 const COLOR_LOW = 'rgb(0, 0, 64)';
 const COLOR_HIGH = 'rgb(255, 255, 64)';
-
+const SCALE_FACTOR = 2;
 function drawDmd(data, x, y, width) {
   var offsetX = 0;
   var offsetY = 0;
@@ -211,8 +214,8 @@ function drawDmd(data, x, y, width) {
       } else {
         c.fillStyle = COLOR_LOW;
       }
-      c.fillRect (x + offsetX, y + offsetY, 1, 1);
-      offsetX ++;
+      c.fillRect(x + offsetX * SCALE_FACTOR, y + offsetY * SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR);
+      offsetX++;
       if (offsetX === width) {
         offsetX = 0;
         offsetY ++;
