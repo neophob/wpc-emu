c6189af2e772fe39cca9b980064cd0ec6fe652cd
michuNEEO <michael@neeo.com>
2018-05-07 23:29:33 +0200

    use milligram css framework

---
+++ webpack.frontend.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const path = require('path');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+
+module.exports = () => {
+  return {
+    entry: './client/client.js',
+    plugins: [
+      new MinifyPlugin()
+    ],
+    output: {
+      filename: 'wpc-client.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [{
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          minimize: true,
+          removeComments: true,
+          collapseWhitespace: true
+        }
+      }]
+    }
+  };
+};
