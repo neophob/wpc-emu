dc007c7a2609f66805c1f0f2f7ac727db54d398b
michuNEEO <michael@neeo.com>
2018-05-08 20:31:46 +0200

    more client update, create switch definition per game

---
+++ client/scripts/ui/controlUi.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import viewTpl from '../main.view.tpl';
+
+import { replaceNode } from './htmlselector';
+export { populateControlUiView };
+
+function addEmulatorControls() {
+  const div = document.createElement('div');
+  div.insertAdjacentHTML('afterbegin', viewTpl);
+  replaceNode('rootNode', div);
+}
+
+function addGameSpecificControls(gameEntry) {
+  const element = document.getElementById('pinball-specfic-switch-input');
+  if (Array.isArray(gameEntry.switchMapping)) {
+    gameEntry.switchMapping.forEach((mapping) => {
+      const child = document.createElement('button');
+      child.innerHTML = mapping.name;
+      child.className = 'button-black button-outline button-small black';
+      child.onclick = () => {
+        window.wpcInterface.wpcSystem.setInput(mapping.id);
+      };
+      element.appendChild(child);
+    });
+  }
+}
+
+function populateControlUiView(gameEntry) {
+  console.log(gameEntry);
+  addEmulatorControls();
+  addGameSpecificControls(gameEntry);
+}
