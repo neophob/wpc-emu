cbea5d973eccc915118735d99c08ff66ddfc9caf
michuNEEO <michael@neeo.com>
2018-05-30 22:59:13 +0200

    try to add a low pass filter with "mixed" results

--- client/scripts/lib/sound.js
+++ client/scripts/lib/sound.js
@@ -8,7 +8,15 @@ function AudioOutput(AudioContext, sampleSize) {
   return new Sound(AudioContext, sampleSize);
 }
 
-const DEFAULT_SAMPLE_SIZE = 1024;
+const DEFAULT_SAMPLE_SIZE = 1024 * 4;
+const INPUT_SAMPLE_RATE_HZ = 11000;
+
+//If a speech signal is sampled at 22050 Hz, the highest frequency that we can expect to be present
+//in the sampled signal is 11025 Hz. This means that to heed this expectation, we should run the
+//continuous signal through a low-pass filter with a cut-off frequency below 11025 Hz; otherwise, we
+//would experience the phenomenon of aliasing.
+
+const CUTOFF_FREQUENCY_HZ = INPUT_SAMPLE_RATE_HZ / 2;
 
 // check https://github.com/bfirsh/jsnes-web/blob/master/src/Speakers.js
 
@@ -18,15 +26,26 @@ class Sound {
     this.audioCtx = new AudioContext();
 
     // TODO replace me with audio workers
+
+    // create elements
     this.audioSourceNode = this.audioCtx.createScriptProcessor(sampleSize, 1, 1);
     this.audioSourceNode.onaudioprocess = this.onaudioprocess.bind(this);
     this.gainNode = this.audioCtx.createGain();
-    this.gainNode.gain.value = 0.7;
+    this.lowpassFilter = this.audioCtx.createBiquadFilter();
 
-    this.audioSourceNode.connect(this.gainNode);
+    // configure nodes
+    this.gainNode.gain.value = 0.9;
+    this.lowpassFilter.type = 'lowpass';
+    this.lowpassFilter.frequency.value = CUTOFF_FREQUENCY_HZ;
+    this.lowpassFilter.Q.value = 10;
+
+
+    // connect nodes
+    this.audioSourceNode.connect(this.lowpassFilter);
+    this.lowpassFilter.connect(this.gainNode);
     this.gainNode.connect(this.audioCtx.destination);
 
-    this.bufferSize = 8192;
+    this.bufferSize = DEFAULT_SAMPLE_SIZE * 2;
     this.buffer = new RingBuffer(this.bufferSize);
     console.log('sound init', this.buffer.size(), this.audioCtx.sampleRate);
   }
@@ -37,6 +56,7 @@ class Sound {
 
     try {
       const samples = this.buffer.deqN(size);
+
       for (var i = 0; i < size; i++) {
         output[i] = samples[i];
       }
@@ -61,11 +81,10 @@ class Sound {
       console.log('Buffer overrun');
     }
     //poor man resampling from 11khz to 44khz
-
     this.buffer.enq(value);
-    this.buffer.enq(value);
-    this.buffer.enq(value);
-    this.buffer.enq(value);
+    this.buffer.enq(0);
+    this.buffer.enq(0);
+    this.buffer.enq(0);
   }
 
   setVolume(floatVolume) {
