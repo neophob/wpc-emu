9fd0b62e05af755706c8ef429be54d82e5e760eb
Michael Vogt <michael@neeo.com>
2018-10-06 15:35:05 +0200

    expose emu version function

---
+++ test/lib/emulator.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import test from 'ava';
+import Emulator from '../../lib/emulator';
+const Package = require('../../package.json');
+
+test('Emulator get version', (t) => {
+  return Emulator.initVMwithRom({
+    u06: new Uint8Array(262144),
+  }).then((emulator) => {
+    const version = emulator.version();
+    t.is(version, Package.version);
+  });
+});
