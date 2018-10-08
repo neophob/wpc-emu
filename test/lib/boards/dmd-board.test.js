db9be4cca8960a6a77f6205337d8256d59fc0def
Michael Vogt <michael@neeo.com>
2018-10-09 00:10:51 +0200

    more dmd tests

--- test/lib/boards/dmd-board.test.js
+++ test/lib/boards/dmd-board.test.js
@@ -28,48 +28,70 @@ test('dmd, should read to WPC_DMD_SCANLINE', (t) => {
 
 test('dmd, should map WPC_DMD_LOW_PAGE', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC_DMD_LOW_PAGE, 2);
+  dmd.write(DMD.OP.WPC_DMD_LOW_PAGE, 2);
   t.is(dmd.dmdPageMapping[0], 2);
 });
 
 test('dmd, should map WPC_DMD_LOW_PAGE, wrap value', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC_DMD_LOW_PAGE, 0xFF);
+  dmd.write(DMD.OP.WPC_DMD_LOW_PAGE, 0xFF);
   t.is(dmd.dmdPageMapping[0], 0xF);
 });
 
 test('dmd, should map WPC_DMD_HIGH_PAGE', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC_DMD_HIGH_PAGE, 3);
+  dmd.write(DMD.OP.WPC_DMD_HIGH_PAGE, 3);
   t.is(dmd.dmdPageMapping[1], 3);
 });
 
 test('dmd, should map WPC95_DMD_PAGE3000', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC95_DMD_PAGE3000, 5);
+  dmd.write(DMD.OP.WPC95_DMD_PAGE3000, 5);
   t.is(dmd.dmdPageMapping[2], 5);
 });
 
 test('dmd, should map WPC95_DMD_PAGE3200', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC95_DMD_PAGE3200, 6);
+  dmd.write(DMD.OP.WPC95_DMD_PAGE3200, 6);
   t.is(dmd.dmdPageMapping[3], 6);
 });
 
 test('dmd, should map WPC95_DMD_PAGE3400', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC95_DMD_PAGE3400, 7);
+  dmd.write(DMD.OP.WPC95_DMD_PAGE3400, 7);
   t.is(dmd.dmdPageMapping[4], 7);
 });
 
 test('dmd, should map WPC95_DMD_PAGE3600', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC95_DMD_PAGE3600, 8);
+  dmd.write(DMD.OP.WPC95_DMD_PAGE3600, 8);
   t.is(dmd.dmdPageMapping[5], 8);
 });
 
 test('dmd, should write next active page, wrap around', (t) => {
   const dmd = t.context;
-  const result = dmd.write(DMD.OP.WPC_DMD_ACTIVE_PAGE, 0xFF);
+  dmd.write(DMD.OP.WPC_DMD_ACTIVE_PAGE, 0xFF);
   t.is(dmd.nextActivePage, 0xF);
+});
+
+[
+  {
+    offset: 0x3800,
+    command: DMD.OP.WPC_DMD_LOW_PAGE,
+  },
+  {
+    offset: 0x3A00,
+    command: DMD.OP.WPC_DMD_HIGH_PAGE,
+  },
+].forEach((testSet) => {
+  test('dmd, should write to DMD RAM ' + testSet.offset, (t) => {
+    const dmd = t.context;
+    const PAGE = 2;
+    const VALUE = 0xFE;
+    dmd.write(testSet.command, PAGE);
+    dmd.write(testSet.offset, VALUE);
+    const result = dmd.read(testSet.offset);
+    t.is(result, VALUE);
+    t.is(dmd.videoRam[0x200 * PAGE], VALUE);
+  });
 });
