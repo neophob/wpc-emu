432b0274ae0b6c3b8d1eeafeaed54885601961b1
Michael Vogt <michael@neeo.com>
2018-10-09 21:23:40 +0200

    implement all dmd mappings

--- test/lib/boards/mapper/dmd.test.js
+++ test/lib/boards/mapper/dmd.test.js
@@ -7,7 +7,7 @@ test('DmdMapper, should get address, 0x3000', (t) => {
   const expectedResult = {
     offset: 0,
     subsystem: 'videoram',
-    page: 2,
+    bank: 2,
   };
   const result = DmdMapper.getAddress(0x3000);
   t.deepEqual(result, expectedResult);
@@ -17,7 +17,7 @@ test('DmdMapper, should get address, 0x3200', (t) => {
   const expectedResult = {
     offset: 0,
     subsystem: 'videoram',
-    page: 3,
+    bank: 3,
   };
   const result = DmdMapper.getAddress(0x3200);
   t.deepEqual(result, expectedResult);
@@ -27,7 +27,7 @@ test('DmdMapper, should get address, 0x3400', (t) => {
   const expectedResult = {
     offset: 0,
     subsystem: 'videoram',
-    page: 4,
+    bank: 4,
   };
   const result = DmdMapper.getAddress(0x3400);
   t.deepEqual(result, expectedResult);
@@ -37,7 +37,7 @@ test('DmdMapper, should get address, 0x3600', (t) => {
   const expectedResult = {
     offset: 0,
     subsystem: 'videoram',
-    page: 5,
+    bank: 5,
   };
   const result = DmdMapper.getAddress(0x3600);
   t.deepEqual(result, expectedResult);
@@ -47,7 +47,7 @@ test('DmdMapper, should get address, 0x3800', (t) => {
   const expectedResult = {
     offset: 0,
     subsystem: 'videoram',
-    page: 0,
+    bank: 0,
   };
   const result = DmdMapper.getAddress(0x3800);
   t.deepEqual(result, expectedResult);
@@ -57,7 +57,7 @@ test('DmdMapper, should get address, 0x3A00', (t) => {
   const expectedResult = {
     offset: 0,
     subsystem: 'videoram',
-    page: 1,
+    bank: 1,
   };
   const result = DmdMapper.getAddress(0x3A00);
   t.deepEqual(result, expectedResult);
@@ -67,7 +67,7 @@ test('DmdMapper, should get address, 0x3A00, should calculate offset correct', (
   const expectedResult = {
     offset: 1,
     subsystem: 'videoram',
-    page: 1,
+    bank: 1,
   };
   const result = DmdMapper.getAddress(0x3A01);
   t.deepEqual(result, expectedResult);
@@ -76,7 +76,7 @@ test('DmdMapper, should get address, 0x3A00, should calculate offset correct', (
 test('DmdMapper, should get address, 0x3FB9', (t) => {
   const expectedResult = {
     offset: 0x3FB9,
-    page: undefined,
+    bank: undefined,
     subsystem: 'command',
   };
   const result = DmdMapper.getAddress(0x3FB9);
