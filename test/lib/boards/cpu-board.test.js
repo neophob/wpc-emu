3bc45f3c94f309effb0b1df8c3aa7548e924b9bf
michuNEEO <michael@neeo.com>
2018-10-18 20:17:20 +0200

    fix bagebank issues

--- test/lib/boards/cpu-board.test.js
+++ test/lib/boards/cpu-board.test.js
@@ -11,7 +11,7 @@ test.beforeEach((t) => {
   const initObject = {
     romObject: '',
     romSizeMBit: 1,
-    systemRom: new Uint8Array(PAGESIZE).fill(0xFF),
+    systemRom: new Uint8Array(2 * PAGESIZE).fill(0xFF),
     fileName: 'foo',
     gameRom,
   };
@@ -58,7 +58,7 @@ test('should _bankswitchedRead, bank 6 (systemrom)', (t) => {
 test('should _bankswitchedRead, bank 7 (systemrom)', (t) => {
   const BANK = 7;
   const cpuBoard = t.context;
-  cpuBoard.systemRom[0] = 12;
+  cpuBoard.systemRom[PAGESIZE] = 12;
   cpuBoard.asic.write(WPC_ROM_BANK, BANK);
   const result = cpuBoard._bankswitchedRead(0);
   t.is(result, 12);
@@ -76,7 +76,7 @@ test('should _bankswitchedRead, bank 8', (t) => {
 test('should _bankswitchedRead, bank 0xFF (systemrom)', (t) => {
   const BANK = 0xFF;
   const cpuBoard = t.context;
-  cpuBoard.systemRom[0] = 12;
+  cpuBoard.systemRom[PAGESIZE] = 12;
   cpuBoard.asic.write(WPC_ROM_BANK, BANK);
   const result = cpuBoard._bankswitchedRead(0);
   t.is(result, 12);
