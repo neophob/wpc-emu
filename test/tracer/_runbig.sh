8e1278c0a24c6a64d3f2d2335953eb1571bd951d
michuNEEO <michael@neeo.com>
2018-10-19 08:59:24 +0200

    add/update scripts to build dump files

--- test/tracer/_runbig.sh
+++ test/tracer/_runbig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "UPDATE 0.3s ROM DUMPS (wpc-emu)"
+echo "UPDATE 2s ROM DUMPS (wpc-emu-dump)"
 
 echo "create WPC-EMU dump files, make sure the following roms exists in the rom directory:"
 echo "- HURCNL_2.ROM (Hurricane)"
@@ -9,12 +9,10 @@ echo "- john1_2r.rom (Johnny Mnemonic)"
 
 echo "DUMP in progress..."
 
-STEPS=600000
+STEPS=4000000
 
 # NOTE: because i'm lazy, tracer writes to stdout - so console.log of the emu do not influence the output
-env STEPS=$STEPS node index.js 2> dump/HURCNL_2_wpc.dump&
-env ROMFILE=../../rom/ftz1_00.rom STEPS=$STEPS node index.js 2> dump/ftz1_00_wpc.dump&
-env ROMFILE=../../rom/john1_2r.rom HAS_SECURITY_FEATURE=true STEPS=$STEPS node index.js 2> dump/john1_2r_wpc.dump&
+env STEPS=$STEPS node index.js 2> wpc-emu-dumps/HURCNL_2_wpc.dump
+env ROMFILE=../../rom/ftz1_00.rom STEPS=$STEPS node index.js 2> wpc-emu-dumps/ftz1_00_wpc.dump
+env ROMFILE=../../rom/john1_2r.rom HAS_SECURITY_FEATURE=true STEPS=$STEPS node index.js 2> wpc-emu-dumps/john1_2r_wpc.dump
 echo "DONE!"
-
-echo "run git diff ."
