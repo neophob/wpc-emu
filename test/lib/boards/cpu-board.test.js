61b30be69c4607bbf28107c97e3ea23778195c38
michuNEEO <michael@neeo.com>
2018-11-22 13:19:03 +0100

    update cpu board tests

--- test/lib/boards/cpu-board.test.js
+++ test/lib/boards/cpu-board.test.js
@@ -18,6 +18,54 @@ test.beforeEach((t) => {
   t.context = CpuBoard.getInstance(initObject);
 });
 
+test('should get ui data', (t) => {
+  const cpuBoard = t.context;
+  cpuBoard.reset();
+  const result = cpuBoard.getUiState();
+  t.is(result.ticks, 0);
+  t.is(result.missedIrqCall, 0);
+  t.is(result.missedFirqCall, 0);
+  t.is(result.irqCount, 0);
+  t.is(result.firqCount, 0);
+  t.is(result.nmiCount, 0);
+  t.is(result.protectedMemoryWriteAttempts, 0);
+});
+
+test('should start cpu board', (t) => {
+  const cpuBoard = t.context;
+  cpuBoard.start();
+  const result = cpuBoard.getUiState();
+  t.is(result.ticks, 0);
+});
+
+test('should change cabinet input', (t) => {
+  const cpuBoard = t.context;
+  cpuBoard.start();
+  cpuBoard.setCabinetInput(1);
+  const state = cpuBoard.getUiState();
+  const result = state.asic.wpc.inputState;
+  t.is(result[0], 1);
+});
+
+test('should change switch input', (t) => {
+  const cpuBoard = t.context;
+  cpuBoard.start();
+  cpuBoard.setInput(11);
+  cpuBoard.setInput(13);
+  const state = cpuBoard.getUiState();
+  const result = state.asic.wpc.inputState;
+  t.is(result[1], 5);
+});
+
+test('should change fliptronice input', (t) => {
+  const cpuBoard = t.context;
+  cpuBoard.start();
+  cpuBoard.setFliptronicsInput('F1');
+  const state = cpuBoard.getUiState();
+  const result = state.asic.wpc.inputState;
+  t.is(result[9], 1);
+});
+
 test('should _bankswitchedRead, bank 0', (t) => {
   const BANK = 0;
   const cpuBoard = t.context;
