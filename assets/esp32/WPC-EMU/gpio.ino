980fa9071b0672ea88c9e7840050390c91966d5d
michuNEEO <michael@neeo.com>
2018-12-25 15:21:37 +0100

    firmware: read switch inputs when irq happens

--- assets/esp32/WPC-EMU/gpio.ino
+++ assets/esp32/WPC-EMU/gpio.ino
@@ -65,12 +65,64 @@ void IRAM_ATTR handleResetInterrupt() {
 
 portMUX_TYPE muxZeroconf = portMUX_INITIALIZER_UNLOCKED;
 volatile uint32_t zeroconfInterruptCounter = 0;
+portMUX_TYPE muxSwitchColumn = portMUX_INITIALIZER_UNLOCKED;
+volatile uint32_t switchColumn = 0;
 
 void IRAM_ATTR handleZerocrossInterrupt() {
+    // ZeroCross should occur 120 times per second (NTSC running at 60Hz)
     portENTER_CRITICAL_ISR(&muxZeroconf);
     zeroconfInterruptCounter++;
     portEXIT_CRITICAL_ISR(&muxZeroconf);
 }
+
+void IRAM_ATTR column1Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 0;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
+void IRAM_ATTR column2Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 1;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
+void IRAM_ATTR column3Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 2;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
+void IRAM_ATTR column4Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 3;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
+void IRAM_ATTR column5Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 4;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
+void IRAM_ATTR column6Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 5;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
+void IRAM_ATTR column7Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 6;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
+void IRAM_ATTR column8Interrupt() {
+    portENTER_CRITICAL_ISR(&muxSwitchColumn);
+    switchColumn = 7;
+    portEXIT_CRITICAL_ISR(&muxSwitchColumn);
+}
+
 
 // NOTE: use INPUT_PULLUP instead INPUT: when no signal is applied, it will be at a voltage level 
 //       of VCC instead of floating, avoiding the detection of non existing external interrupts.
@@ -79,14 +131,34 @@ void initGpio() {
     pinMode(GPIO_RO_ZEROCROSS, INPUT_PULLDOWN);
     attachInterrupt(digitalPinToInterrupt(GPIO_RO_ZEROCROSS), handleZerocrossInterrupt, FALLING);
 
+
+    //TODO
+    // - keep data in memory to build up a 8x8 matrix
+    // - validate that active colum switches timing match the esp timing or if we need in interrupt
+    
     pinMode(GPIO_RO_ACTIVE_COLUMN_1, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_1), column1Interrupt, RISING);
+
     pinMode(GPIO_RO_ACTIVE_COLUMN_2, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_2), column2Interrupt, RISING);
+
     pinMode(GPIO_RO_ACTIVE_COLUMN_3, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_3), column3Interrupt, RISING);
+
     pinMode(GPIO_RO_ACTIVE_COLUMN_4, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_4), column4Interrupt, RISING);
+
     pinMode(GPIO_RO_ACTIVE_COLUMN_5, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_5), column5Interrupt, RISING);
+
     pinMode(GPIO_RO_ACTIVE_COLUMN_6, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_6), column6Interrupt, RISING);
+
     pinMode(GPIO_RO_ACTIVE_COLUMN_7, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_7), column7Interrupt, RISING);
+
     pinMode(GPIO_RO_ACTIVE_COLUMN_8, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(GPIO_RO_ACTIVE_COLUMN_8), column8Interrupt, RISING);
     
     pinMode(GPIO_RO_SWITCH_INPUT_1, INPUT_PULLDOWN);
     pinMode(GPIO_RO_SWITCH_INPUT_2, INPUT_PULLDOWN);
@@ -114,8 +186,7 @@ void resetPinballMachine() {
     initialiseResetInput();
 }
 
-uint8_t activeColumn = 0;
-uint8_t switchInput = 0;
+uint8_t switchState[8] = { 0,0,0,0,0,0,0,0 };
 
 void loopGPIO() {
   if (resetInterruptCounter > 0) {
@@ -126,15 +197,16 @@ void loopGPIO() {
   // reading 2 x 8 pins takes 4us on the devboard
   
   unsigned long now = micros();
-  bitWrite(activeColumn, 0, digitalRead(GPIO_RO_ACTIVE_COLUMN_1));
+/*  bitWrite(activeColumn, 0, digitalRead(GPIO_RO_ACTIVE_COLUMN_1));
   bitWrite(activeColumn, 1, digitalRead(GPIO_RO_ACTIVE_COLUMN_2));
   bitWrite(activeColumn, 2, digitalRead(GPIO_RO_ACTIVE_COLUMN_3));
   bitWrite(activeColumn, 3, digitalRead(GPIO_RO_ACTIVE_COLUMN_4));
   bitWrite(activeColumn, 4, digitalRead(GPIO_RO_ACTIVE_COLUMN_5));
   bitWrite(activeColumn, 5, digitalRead(GPIO_RO_ACTIVE_COLUMN_6));
   bitWrite(activeColumn, 6, digitalRead(GPIO_RO_ACTIVE_COLUMN_7));
-  bitWrite(activeColumn, 7, digitalRead(GPIO_RO_ACTIVE_COLUMN_8));
+  bitWrite(activeColumn, 7, digitalRead(GPIO_RO_ACTIVE_COLUMN_8));*/
 
+  uint8_t switchInput = 0;
   bitWrite(switchInput, 0, digitalRead(GPIO_RO_SWITCH_INPUT_1));
   bitWrite(switchInput, 1, digitalRead(GPIO_RO_SWITCH_INPUT_2));
   bitWrite(switchInput, 2, digitalRead(GPIO_RO_SWITCH_INPUT_3));
@@ -143,6 +215,8 @@ void loopGPIO() {
   bitWrite(switchInput, 5, digitalRead(GPIO_RO_SWITCH_INPUT_6));
   bitWrite(switchInput, 6, digitalRead(GPIO_RO_SWITCH_INPUT_7));
   bitWrite(switchInput, 7, digitalRead(GPIO_RO_SWITCH_INPUT_8));
+  switchState[ switchColumn ] = switchInput;
+  
   unsigned long now2 = micros();
-  Serial.printf("activeColumn: %d, switchInput: %d, duration: %lu us\n", activeColumn, switchInput, (unsigned long)now2-now);  
+  Serial.printf("switchColumn: %d, switchInput: %d, duration: %lu us\n", switchColumn, switchInput, (unsigned long)now2-now);  
 }
---
+++ assets/esp32/WPC-EMU/timing.ino
@@ -0,0 +1,36 @@
+/*
+
+# debug output 
+
+log output from wpc emu, timings are not 
+
+## SWITCH MATRIX
+
+2018-12-22T15:27:34.605Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 0
+2018-12-22T15:27:34.606Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 1
+2018-12-22T15:27:34.606Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 2
+2018-12-22T15:27:34.606Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 3
+2018-12-22T15:27:34.607Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 4
+2018-12-22T15:27:34.607Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 5
+2018-12-22T15:27:34.607Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 6
+2018-12-22T15:27:34.607Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 7
+
+2018-12-22T15:27:34.607Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 0
+2018-12-22T15:27:34.608Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 1
+2018-12-22T15:27:34.608Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 2
+2018-12-22T15:27:34.608Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 3
+2018-12-22T15:27:34.608Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 4
+2018-12-22T15:27:34.608Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 5
+2018-12-22T15:27:34.608Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 6
+2018-12-22T15:27:34.608Z wpcemu:boards:elements:inputSwitchMatrix SET ACTIVE_COLUMN 7
+
+
+# ZEROCROSS
+
+2018-12-22T15:26:34.895Z wpcemu:boards:asic WRITE WPC_ZEROCROSS_IRQ_CLEAR: WPC_ZEROCROSS_IRQ_CLEAR 150
+2018-12-22T15:26:34.896Z wpcemu:boards:asic WRITE WPC_ZEROCROSS_IRQ_CLEAR: WPC_ZEROCROSS_IRQ_CLEAR 150
+2018-12-22T15:26:34.897Z wpcemu:boards:asic WRITE WPC_ZEROCROSS_IRQ_CLEAR: WPC_ZEROCROSS_IRQ_CLEAR 150
+2018-12-22T15:26:34.898Z wpcemu:boards:asic WRITE WPC_ZEROCROSS_IRQ_CLEAR: WPC_ZEROCROSS_IRQ_CLEAR 150
+2018-12-22T15:26:34.900Z wpcemu:boards:asic WRITE WPC_ZEROCROSS_IRQ_CLEAR: WPC_ZEROCROSS_IRQ_CLEAR 150
+
+ */
