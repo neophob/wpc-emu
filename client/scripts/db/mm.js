d89857a01ee52374e9fc74f2e28fe64ce3197671
michuNEEO <michael@neeo.com>
2019-01-11 17:48:41 +0100

    fix some default game switches

--- client/scripts/db/mm.js
+++ client/scripts/db/mm.js
@@ -77,7 +77,7 @@ module.exports = {
   ],
   initialise: {
     closedSwitches: [
-      22,
+      22, 56,
       //OPTO SWITCHES: 31, 32, 33, 34, 35, 36, 37, 41,
       31, 36, 37, 41,
       'F2', 'F4', 'F6', 'F8',
