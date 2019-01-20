7d313d6253ea98de54a2e34662de0df6627a1a7a
michuNEEO <michael@neeo.com>
2019-01-20 11:27:31 +0100

    tom: close coin door per default

--- client/scripts/db/tom.js
+++ client/scripts/db/tom.js
@@ -84,8 +84,9 @@ module.exports = {
     'securityPic',
   ],
   initialise: {
-    //OPTO SWITCHES: 31, 32, 33, 34, 35, 36, 55, 56, 57, 58
     closedSwitches: [
+      22,
+      //OPTO SWITCHES: 31, 32, 33, 34, 35, 36, 55, 56, 57, 58
       36, 55, 56, 57, 58,
       'F2', 'F4',
     ],
