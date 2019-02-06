545d768283dd2abd2e54e16a2352b8fff559006b
michuNEEO <michael@neeo.com>
2019-02-06 21:48:18 +0100

    soundboard is aware what hardware is used, update rom loader

--- test/lib/rom/index.test.js
+++ test/lib/rom/index.test.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 import romParser from '../../../lib/rom/index';
 
-test('romParser should reject invalid data', (t) => {
+test('romParser should reject empty rom', (t) => {
   return romParser
     .parse()
     .catch((error) => {
@@ -11,27 +11,74 @@ test('romParser should reject invalid data', (t) => {
     });
 });
 
-test('romParser should parse roms', (t) => {
-  const u06 = new Uint8Array(256 * 1024).fill(11);
-  const u14 = new Uint8Array(0x4000).fill(22);
-  const u15 = new Uint8Array(0x4000).fill(33);
-  const u18 = new Uint8Array(0x4000).fill(44);
+test('romParser should reject invalid data', (t) => {
   const romData = {
-    u06,
-    u14,
-    u15,
-    u18,
+    u06: new Uint8Array(7),
   };
   return romParser
     .parse(romData)
+    .catch((error) => {
+      t.is(error.message, 'INVALID_ROM_SIZE');
+    });
+});
+
+test('romParser should parse game rom', (t) => {
+  const romData = buildRomData();
+  return romParser
+    .parse(romData)
     .then((result) => {
-      t.is(result.concatenatedSoundRom[0x000000], 44);
-      t.is(result.concatenatedSoundRom[0x080000], 33);
-      t.is(result.concatenatedSoundRom[0x100000], 22);
       t.is(result.gameRom[0x000000], 11);
       t.is(result.fileName, 'Unknown');
       t.is(result.romSizeMBit, 2);
       t.is(result.skipWmcRomCheck, false);
       t.is(result.hasSecurityPic, false);
+      t.is(result.preDcsSoundboard, false);
     });
 });
+
+test('romParser should parse wpc95 board', (t) => {
+  const romData = buildRomData();
+  const metaData = {
+    features: [ 'wpc95', 'securityPic' ],
+  }
+  return romParser
+    .parse(romData, metaData)
+    .then((result) => {
+      t.is(result.preDcsSoundboard, false);
+      t.is(result.hasSecurityPic, true);
+    });
+});
+
+test('romParser should parse wpcDmd board', (t) => {
+  const romData = buildRomData();
+  const metaData = {
+    features: [ 'wpcDmd' ],
+  }
+  return romParser
+    .parse(romData, metaData)
+    .then((result) => {
+      t.is(result.preDcsSoundboard, true);
+      t.is(result.hasSecurityPic, false);
+    });
+});
+
+test('romParser should parse wpcFliptronics board', (t) => {
+  const romData = buildRomData();
+  const metaData = {
+    features: [ 'wpcFliptronics' ],
+  }
+  return romParser
+    .parse(romData, metaData)
+    .then((result) => {
+      t.is(result.preDcsSoundboard, true);
+      t.is(result.hasSecurityPic, false);
+    });
+});
+
+
+function buildRomData() {
+  const u06 = new Uint8Array(256 * 1024).fill(11);
+  return {
+    u06,
+  }
+}
