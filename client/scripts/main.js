be814a6cefbd9390b5fa4aaf6221d3da1b705804
michuNEEO <michael@neeo.com>
2019-03-20 21:58:01 +0100

    use local webpack

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -248,11 +248,12 @@ if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   // NOTE: works only via SSL!
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js').then(registration => {
-      console.log('SW registered:', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed:', registrationError);
-    });
+    navigator.serviceWorker.register('service-worker.js')
+      .then((registration) => {
+        console.log('SW registered:', registration);
+      }).catch(registrationError => {
+        console.log('SW registration failed:', registrationError);
+      });
   });
 }
 
