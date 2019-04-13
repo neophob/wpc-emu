9236ed0ef1ff0dbf30e604c4c1f976db8e6a5669
michuNEEO <michael@neeo.com>
2019-04-13 23:17:44 +0200

    improve tests

--- client/scripts/lib/rom-uploader.js
+++ client/scripts/lib/rom-uploader.js
@@ -17,32 +17,26 @@ function getUploadedFile() {
 
     romUploadElement.addEventListener('change', () => {
       const files = romUploadElement.files;
-      if (!files || files.length !== 1) {
+      if (!Array.isArray(files) || files.length !== 1) {
         const error = new Error('ABORT');
         return reject(error);
       }
 
-      console.log('Filename:', files[0].name);
-      console.log('Size:', files[0].size + " bytes");
       fileToUint8Array(files[0])
         .then((arrayBuffer) => {
           resolve(arrayBuffer);
         });
     });
-  //TODO unregister event listeners
-
+    //TODO unregister event listeners
     romUploadElement.click();
   });
 }
 
 function fileToUint8Array(file) {
-  console.log('file',file)
-  console.log('type', typeof file)
   return new Promise((resolve) => {
     const fileReader = new FileReader();
     fileReader.onload = () => {
-      const arrayBuffer = fileReader.result
-      resolve(arrayBuffer);
+      resolve(fileReader.result);
     }
     fileReader.readAsArrayBuffer(file);
   });
---
+++ client/test/scripts/lib/rom-uploader.test.js
@@ -0,0 +1,102 @@
+'use strict';
+
+import browserEnv from 'browser-env';
+import test from 'ava';
+import { getUploadedFile } from '../../../scripts/lib/rom-uploader';
+
+const FAKE_FILE_CONTENT = 'ABC';
+
+test.beforeEach((t) => {
+  browserEnv();
+  t.context.callback = [];
+
+  const div = document.createElement('div');
+	document.body.appendChild(div);
+
+  const htmlInput = document.createElement('INPUT');
+  htmlInput.setAttribute('type', 'file');
+  htmlInput.setAttribute('id', 'romUpload');
+
+  div.appendChild(htmlInput);
+
+  htmlInput.addEventListener = function(name, func) {
+    t.context.callback[name] = func;
+  }
+  t.context.htmlInput = htmlInput;
+});
+
+test.serial('getUploadedFile, should abort', (t) => {
+  const promise = getUploadedFile()
+    .catch((error) => {
+      t.is(error.message, 'ABORT');
+    });
+
+  t.context.callback['abort']();
+
+  return promise;
+});
+
+test.serial('getUploadedFile, should throw error', (t) => {
+  const promise = getUploadedFile()
+    .catch((error) => {
+      t.is(error.message, 'foo');
+    });
+
+  t.context.callback.error(new Error('foo'));
+
+  return promise;
+});
+
+test.serial('getUploadedFile, should resolve but no files defined - abort', (t) => {
+  const promise = getUploadedFile()
+    .catch((error) => {
+      t.is(error.message, 'ABORT');
+    });
+
+  t.context.callback.change();
+
+  return promise;
+});
+
+test.serial('getUploadedFile, should resolve', (t) => {
+  const promise = getUploadedFile()
+    .then((value) => {
+      t.deepEqual(value, stringToArrayBuffer(FAKE_FILE_CONTENT));
+    });
+
+  const files = [ 'sample1.txt' ];
+  addFileListToInputElement(t.context.htmlInput, files);
+  t.context.callback.change();
+
+  return promise;
+});
+
+function stringToArrayBuffer(string) {
+  var buf = new ArrayBuffer(string.length); // 2 bytes for each char
+  var bufView = new Uint8Array(buf);
+  for (var i = 0, strLen = string.length; i < strLen; i++) {
+    bufView[i] = string.charCodeAt(i);
+  }
+  return buf;
+}
+
+function addFileListToInputElement(elements, filePaths) {
+  const fileList = filePaths.map(fp => createFile(fp))
+  fileList.__proto__ = Object.create(FileList.prototype)
+
+  Object.defineProperty(elements, 'files', {
+    value: fileList,
+    writable: false,
+  });
+}
+
+function createFile(filePath) {
+  return new File(
+    [FAKE_FILE_CONTENT],
+    filePath,
+    {
+      lastModified: Date.now(),
+      type: 'foo',
+    }
+  )
+}
