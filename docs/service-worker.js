3108337908f4afef11665dca6c4fc013e106fe38
michuNEEO <michael@neeo.com>
2019-04-26 23:04:53 +0200

    bump to v0.22.3

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.06b74a82f1c22fb72a595249f0800372.js"
+  "precache-manifest.c08a58ac753ff3bbd6c6af66b3792f7a.js"
 );
 
 workbox.skipWaiting();
