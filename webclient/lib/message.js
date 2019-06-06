db426df2ec2dfa2defefb0756635465896d49651
michuNEEO <michael@neeo.com>
2019-06-06 23:45:37 +0200

    work on the webclient

---
+++ webclient/lib/message.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const initialiseEmulator = 'initialiseEmulator';
+const pauseEmulator = 'pauseEmulator';
+const resumeEmulator = 'resumeEmulator';
+const resetEmulator = 'resetEmulator';
+
+const ACK = 'ACK';
+
+export {
+  initialiseEmulator,
+  pauseEmulator,
+  resumeEmulator,
+  resetEmulator,
+  ACK,
+};
---
+++ webclient/webworker.js
@@ -0,0 +1,102 @@
+'use strict';
+
+import WpcEmu from '../../lib/emulator.js';
+import * as MESSAGE from './lib/message.js';
+
+let emu;
+
+function initialiseEmulator(romData, gameEntry) {
+  const fileName = gameEntry.rom.u06;
+  const romObject = {
+    fileName,
+    skipWpcRomCheck: gameEntry.skipWpcRomCheck,
+    features: gameEntry.features,
+  };
+  return WpcEmu.initVMwithRom(romData, romObject)
+    .then((wpcSystem) => {
+      console.log('INITIALIZED!');
+      emu = new Emu(wpcSystem);
+    });
+}
+
+
+onmessage = (event) => {
+  if (event.data.length !== 3) {
+    console.error('INVALID_PARAMETER_SIZE');
+    postMessage({ message: 'error', parameter: 'invalid parameter size' });
+    return;
+  }
+
+  postMessage('IN', event);
+
+  const requestId = event.data[0];
+  const message = event.data[1];
+  const parameter = event.data[2];
+
+  console.log(`[From Main]: ${requestId} - ${message} - ${parameter}`);
+
+  switch (message) {
+    case MESSAGE.initialiseEmulator:
+      initialiseEmulator(parameter.romData, parameter.gameEntry)
+        .then(() => {
+          emu.start();
+          postMessage({ message: MESSAGE.ACK, requestId });
+        });
+      break;
+
+    case MESSAGE.pauseEmulator:
+      //TODO implement
+      postMessage({ message: MESSAGE.ACK, requestId });
+      break;
+
+    case MESSAGE.resumeEmulator:
+      //TODO implement
+      postMessage({ message: MESSAGE.ACK, requestId });
+      break;
+
+    case MESSAGE.resetEmulator:
+      //TODO validate if emu is defined
+      emu.reset();
+      postMessage({ message: MESSAGE.ACK, requestId });
+      break;
+
+    default:
+      postMessage({ message: 'error', parameter: 'invalid message: ' + message });
+  }
+
+};
+
+
+class Emu {
+
+  constructor(emuInstance) {
+    this.emuInstance = emuInstance;
+  }
+
+  getVersion() {
+    return this.emuInstance.version();
+  }
+
+  start() {
+    this.emuInstance.start();
+  }
+
+  reset() {
+    this.emuInstance.reset();
+  }
+
+  setCabinetInput(key) {
+    this.emuInstance.setCabinetInput(key);
+  }
+
+  setInput(key) {
+    this.emuInstance.setInput(key);
+  }
+
+  executeCycle(key) {
+    this.emuInstance.executeCycle(TICKS_PER_CALL, TICKS_PER_STEP);
+  }
+
+
+}
+
