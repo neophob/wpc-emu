57d50df5f2f50db2b80250b961d7b6f0c9f48217
michuNEEO <michael@neeo.com>
2019-07-15 23:32:21 +0200

    fix gi state restore, fix gi tests

--- test/lib/boards/elements/output-general-illumination.test.js
+++ test/lib/boards/elements/output-general-illumination.test.js
@@ -27,8 +27,8 @@ test('generalIllumination wpc95, update with value 0', (t) => {
   t.is(generalIllumination.generalIlluminationState[0], 0x00);
   t.is(generalIllumination.generalIlluminationState[1], 0x00);
   t.is(generalIllumination.generalIlluminationState[2], 0x00);
-  t.is(generalIllumination.generalIlluminationState[3], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[4], 0xFF);
+  t.is(generalIllumination.generalIlluminationState[3], 0x07);
+  t.is(generalIllumination.generalIlluminationState[4], 0x07);
   t.is(generalIllumination.generalIlluminationState[5], 0x00);
   t.is(generalIllumination.generalIlluminationState[6], 0x00);
   t.is(generalIllumination.generalIlluminationState[7], 0x00);
@@ -37,11 +37,11 @@ test('generalIllumination wpc95, update with value 0', (t) => {
 test('generalIllumination, update with value 0xFF', (t) => {
   const generalIllumination = t.context.preWpc95;
   generalIllumination.update(0xFF);
-  t.is(generalIllumination.generalIlluminationState[0], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[1], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[2], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[3], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[4], 0xFF);
+  t.is(generalIllumination.generalIlluminationState[0], 0x07);
+  t.is(generalIllumination.generalIlluminationState[1], 0x07);
+  t.is(generalIllumination.generalIlluminationState[2], 0x07);
+  t.is(generalIllumination.generalIlluminationState[3], 0x07);
+  t.is(generalIllumination.generalIlluminationState[4], 0x07);
   t.is(generalIllumination.generalIlluminationState[5], 0x00);
   t.is(generalIllumination.generalIlluminationState[6], 0x00);
   t.is(generalIllumination.generalIlluminationState[7], 0x00);
@@ -50,11 +50,11 @@ test('generalIllumination, update with value 0xFF', (t) => {
 test('generalIllumination wpc95, update with value 0xFF', (t) => {
   const generalIllumination = t.context.wpc95;
   generalIllumination.update(0xFF);
-  t.is(generalIllumination.generalIlluminationState[0], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[1], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[2], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[3], 0xFF);
-  t.is(generalIllumination.generalIlluminationState[4], 0xFF);
+  t.is(generalIllumination.generalIlluminationState[0], 0x07);
+  t.is(generalIllumination.generalIlluminationState[1], 0x07);
+  t.is(generalIllumination.generalIlluminationState[2], 0x07);
+  t.is(generalIllumination.generalIlluminationState[3], 0x07);
+  t.is(generalIllumination.generalIlluminationState[4], 0x07);
   t.is(generalIllumination.generalIlluminationState[5], 0x00);
   t.is(generalIllumination.generalIlluminationState[6], 0x00);
   t.is(generalIllumination.generalIlluminationState[7], 0x00);
@@ -65,8 +65,8 @@ test('generalIllumination, update with values 0x2 and 0x4', (t) => {
   generalIllumination.update(0x2);
   generalIllumination.update(0x4);
   t.is(generalIllumination.generalIlluminationState[0], 0x00);
-  t.is(generalIllumination.generalIlluminationState[1], 0x00);
-  t.is(generalIllumination.generalIlluminationState[2], 0xFF);
+  t.is(generalIllumination.generalIlluminationState[1], 0x06);
+  t.is(generalIllumination.generalIlluminationState[2], 0x07);
   t.is(generalIllumination.generalIlluminationState[3], 0x00);
 });
 
@@ -76,6 +76,14 @@ test('generalIllumination, update with values 0x4 and 0x4', (t) => {
   generalIllumination.update(0x4);
   t.is(generalIllumination.generalIlluminationState[0], 0x00);
   t.is(generalIllumination.generalIlluminationState[1], 0x00);
-  t.is(generalIllumination.generalIlluminationState[2], 0xFF);
+  t.is(generalIllumination.generalIlluminationState[2], 0x07);
   t.is(generalIllumination.generalIlluminationState[3], 0x00);
 });
+
+test('generalIllumination, getUint8ArrayFromState', (t) => {
+  const generalIllumination = t.context.preWpc95;
+  const result = generalIllumination.getUint8ArrayFromState([ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF ]);
+  t.deepEqual(result, Uint8Array.from([8, 8, 8, 8, 8, 8, 8, 8]));
+});
+
+//TODO with multiple rounds
