4351c6295ebc104d206859e2e189e62995fb2ef7
Michael Vogt <mvogt@control4.com>
2019-09-10 13:52:21 +0200

    update minimal example prepare proper facade

--- assets/client-example/scripts/main.js
+++ assets/client-example/scripts/main.js
@@ -1,34 +1,35 @@
 'use strict';
 
+import * as WpcEmu from 'wpc-emu';
+
 // reference the webworker from wep-emu
 import WebWorker from 'worker-loader!wpc-emu/lib/webclient/webworker.js';
-import * as gamelist from 'wpc-emu/client/scripts/db/gamelist';
-import * as WpcEmuWebWorkerApi from 'wpc-emu/lib/webclient';
 
 import { downloadFileFromUrlAsUInt8Array } from './lib/fetcher';
 import { initialiseActions } from './lib/initialise';
 import { initCanvas, drawDmdShaded } from './lib/canvas';
 
-let wpcEmuWebWorkerApi;
-
 const DESIRED_FPS = 50;
 const INITIAL_GAME = 'WPC-DMD: Hurricane';
 
+let wpcEmuWebWorkerApi;
+let counter = 0;
 let rafId;
 
-function initializeEmu(gameEntry) {
+console.log('INIT WPC-EMU-EXAMPLE');
+wpcEmuWebWorkerApi = WpcEmu.WpcEmuWebWorkerApi.initialiseWebworkerAPI(new WebWorker());
+initEmuWithGameName(INITIAL_GAME)
+  .then(() => registerKeyboardListener())
+  .catch((error) => console.error);
 
+function initializeEmu(gameEntry) {
   return downloadFileFromUrlAsUInt8Array(gameEntry.rom.u06)
     .then((u06Rom) => {
       console.log('Successfully loaded ROM', u06Rom.length);
-      const romData = {
-        u06: u06Rom,
-      };
-      return wpcEmuWebWorkerApi.initialiseEmulator(romData, gameEntry);
+      return wpcEmuWebWorkerApi.initialiseEmulator({ u06: u06Rom }, gameEntry);
     })
     .then((emuVersion) => {
       console.log('Successfully initialized emulator', emuVersion);
-
       window.wpcInterface = {
         webclient: wpcEmuWebWorkerApi,
         resetEmu,
@@ -36,7 +37,6 @@ function initializeEmu(gameEntry) {
         resumeEmu,
         writeMemory,
       };
-
       initCanvas();
 
       wpcEmuWebWorkerApi.registerAudioConsumer((message) => {
@@ -50,31 +50,25 @@ function initializeEmu(gameEntry) {
           return;
         }
         if (rafId) {
-          //console.log('MISSED_DRAW!', rafId, missedDraw);
+          console.log('MISSED_DRAW!', rafId);
           cancelAnimationFrame(rafId);
         }
 
-        rafId = requestAnimationFrame((timestamp) => {
-
-          if (emuState.asic.dmd.dmdShadedBuffer) {
+        if (emuState.asic.dmd.dmdShadedBuffer) {
+          rafId = requestAnimationFrame((timestamp) => {
             drawDmdShaded(emuState.asic.dmd.dmdShadedBuffer);
-          }
-          //console.log('wpcEmuWebWorkerApi.getStatistics()',wpcEmuWebWorkerApi.getStatistics());
-/*          const { averageRTTms, sentMessages, failedMessages } = wpcEmuWebWorkerApi.getStatistics();
-          emuDebugUi.drawMetaData({
-            averageRTTms, sentMessages, failedMessages, missedDraw, lastFps
+            rafId = 0;
           });
-*/
-          rafId = 0;
-        });
-
+        }
+        if ((++counter % 240) === 0) {
+          console.log('wpcEmuWebWorkerApi.getStatistics()',wpcEmuWebWorkerApi.getStatistics());
+        }
       });
     });
 }
 
 function initEmuWithGameName(name) {
-  //soundInstance.stop();
-  const gameEntry = gamelist.getByName(name);
+  const gameEntry = WpcEmu.gamelist.getByName(name);
 
   return Promise.all([ initializeEmu(gameEntry), wpcEmuWebWorkerApi.reset() ])
     .then(resumeEmu)
@@ -113,56 +107,26 @@ function registerKeyboardListener() {
     switch (e.keyCode) {
       case 49: //1
         return wpcEmuWebWorkerApi.setCabinetInput(1);
-
       case 50: //2
         return wpcEmuWebWorkerApi.setCabinetInput(2);
-
       case 51: //3
         return wpcEmuWebWorkerApi.setCabinetInput(4);
-
       case 52: //4
         return wpcEmuWebWorkerApi.setCabinetInput(8);
-
       case 53: //5
         return wpcEmuWebWorkerApi.setInput(13);
-
       case 55: //7
         return wpcEmuWebWorkerApi.setCabinetInput(16);
-
       case 56: //8
         return wpcEmuWebWorkerApi.setCabinetInput(32);
-
       case 57: //9
         return wpcEmuWebWorkerApi.setCabinetInput(64);
-
       case 48: //0
         return wpcEmuWebWorkerApi.setCabinetInput(128);
-
       case 80: //P
         return pauseEmu();
-
       case 82: //R
         return resumeEmu();
     }
   }, false);
 }
-
-console.log('INIT WPC-EMU-EXAMPLE');
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  // NOTE: works only via SSL!
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
-      .then((registration) => {
-        console.log('SW registered:', registration);
-      }).catch((error) => {
-        console.log('SW registration failed:', error);
-      });
-  });
-}
-
-wpcEmuWebWorkerApi = WpcEmuWebWorkerApi.initialiseWebworkerAPI(new WebWorker());
-initEmuWithGameName(INITIAL_GAME)
-  .catch((error) => console.error);
-
-registerKeyboardListener();
