81084d955610c3af2d665be200295d0611e3796a
michuNEEO <michael@neeo.com>
2019-10-29 12:56:01 +0100

    speed up build on osx fix github ci config

--- build/docker-wrapper.sh
+++ build/docker-wrapper.sh
@@ -6,16 +6,15 @@ ABSOLUTE_ROOT="$( cd "$(dirname "$0")/.." ; pwd -P )" 2>/dev/null
 DOCKER_IMAGE="node:10-jessie-slim"
 
 if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
-    echo "ERROR: Release aborted, local changes detected."
-    #exit 1
+    echo "⚠️  WARNING: local changes detected."
 fi
 
 echo "INFO: Starting DOCKER Release $ABSOLUTE_ROOT"
 
 EXECUTE_BUILDSCRIPT="cd /source/build && ./release.sh $1"
 time docker run --rm \
-           -a stdin -a stdout -a stderr \
-           -v ${ABSOLUTE_ROOT}:/source \
+           -a stdout -a stderr \
+           -v ${ABSOLUTE_ROOT}:/source:cached \
            $DOCKER_IMAGE bash -c "${EXECUTE_BUILDSCRIPT}"
 
 echo "INFO: 👷 LGTM!"
