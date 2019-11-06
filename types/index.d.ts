0e355d3ab0ee2e4a30d5373c9d022400fb5723a9
michuNEEO <michael@neeo.com>
2019-11-06 23:07:43 +0100

    update type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -187,10 +187,11 @@ export namespace WpcEmuApi {
     setCabinetInput(value: number): void;
 
     /**
-     * Toggle a switch
+     * Set or toggle a switch
      * @param switchNr number between 11 and 99
+     * @param optionalValue if undefined, switch will toggle, else set to the defined state
      */
-    setInput(switchNr: number): void;
+    setSwitchInput(switchNr: number, optionalValue: boolean): void;
 
     /**
      * Fliptronic flipper move (depends on the machine if this is supported)
