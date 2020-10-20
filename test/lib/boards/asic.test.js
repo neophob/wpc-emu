ea6fde8569e4ff5fe217cd0b2ec0170cf9a6ff08
Michael <michu@neophob.com>
2020-10-20 21:16:10 +0200

    simplify read code

--- test/lib/boards/asic.test.js
+++ test/lib/boards/asic.test.js
@@ -170,6 +170,13 @@ test('wpc, write and read fliptronics', (t) => {
   t.is(result, 247);
 });
 
+test('wpc, write and read wpc95 flipper', (t) => {
+  const wpc = t.context;
+  wpc.setFliptronicsInput('F4');
+  const result = wpc.read(CpuBoardAsic.OP.WPC95_FLIPPER_SWITCH_INPUT);
+  t.is(result, 247);
+});
+
 test('wpc, ignore empty setState', (t) => {
   const wpc = t.context;
   const result = wpc.setState();
