d252d641c7b596ae9081b1e42f16bfcd968abc7a
Michael Vogt <michu@neophob.com>
2023-03-19 22:01:45 +0100

    minor lint fixes

--- test/lib/boards/up/cpu6809.dsl.test.js
+++ test/lib/boards/up/cpu6809.dsl.test.js
@@ -399,7 +399,7 @@ function flagCheckTest(t, testData) {
   cpu.set('flags', 0xFF);
   cpu.step();
 
-  for (var x = 0; x < testData.flags.length; x++) {
+  for (let x = 0; x < testData.flags.length; x++) {
     const flag = testData.flags[x];
     const mask = EXPECTED_FLAG_MAP[x];
     switch (flag) {
